e79b3a63ce5f839d910bfd84cb21fcd85153722a
Philip Sadkov <philip.sadkov@gmail.com>
2020-03-08 21:59:36 +0300

    Update lycheesyncer.py

--- lycheesync/lycheesyncer.py
+++ lycheesync/lycheesyncer.py
@@ -216,39 +216,42 @@ class LycheeSyncer:
         if photo.exif.orientation != 1:
 
             img = Image.open(photo.destfullpath)
-            if "exif" in img.info:
-                exif_dict = piexif.load(img.info["exif"])
+            try:
+                if "exif" in img.info:
+                    exif_dict = piexif.load(img.info["exif"])
 
-                if piexif.ImageIFD.Orientation in exif_dict["0th"]:
-                    orientation = exif_dict["0th"][piexif.ImageIFD.Orientation]
+                    if piexif.ImageIFD.Orientation in exif_dict["0th"]:
+                        orientation = exif_dict["0th"][piexif.ImageIFD.Orientation]
 
-                    if orientation == 2:
-                        img = img.transpose(Image.FLIP_LEFT_RIGHT)
-                    elif orientation == 3:
-                        img = img.rotate(180)
-                    elif orientation == 4:
-                        img = img.rotate(180).transpose(Image.FLIP_LEFT_RIGHT)
-                    elif orientation == 5:
-                        img = img.rotate(-90, expand=True).transpose(Image.FLIP_LEFT_RIGHT)
-                    elif orientation == 6:
-                        img = img.rotate(-90, expand=True)
-                    elif orientation == 7:
-                        img = img.rotate(90, expand=True).transpose(Image.FLIP_LEFT_RIGHT)
-                    elif orientation == 8:
-                        img = img.rotate(90, expand=True)
-                    else:
-                        if orientation != 1:
-                            logger.warn("Orientation not defined {} for photo {}".format(orientation, photo.title))
+                        if orientation == 2:
+                            img = img.transpose(Image.FLIP_LEFT_RIGHT)
+                        elif orientation == 3:
+                            img = img.rotate(180)
+                        elif orientation == 4:
+                            img = img.rotate(180).transpose(Image.FLIP_LEFT_RIGHT)
+                        elif orientation == 5:
+                            img = img.rotate(-90, expand=True).transpose(Image.FLIP_LEFT_RIGHT)
+                        elif orientation == 6:
+                            img = img.rotate(-90, expand=True)
+                        elif orientation == 7:
+                            img = img.rotate(90, expand=True).transpose(Image.FLIP_LEFT_RIGHT)
+                        elif orientation == 8:
+                            img = img.rotate(90, expand=True)
+                        else:
+                            if orientation != 1:
+                                logger.warn("Orientation not defined {} for photo {}".format(orientation, photo.title))
 
-                    if orientation in [5, 6, 7, 8]:
-                        # invert width and height
-                        h = photo.height
-                        w = photo.width
-                        photo.height = w
-                        photo.width = h
-                    exif_dict["0th"][piexif.ImageIFD.Orientation] = 1
-                    exif_bytes = piexif.dump(exif_dict)
-                    img.save(photo.destfullpath, exif=exif_bytes, quality=99)
+                        if orientation in [5, 6, 7, 8]:
+                            # invert width and height
+                            h = photo.height
+                            w = photo.width
+                            photo.height = w
+                            photo.width = h
+                        exif_dict["0th"][piexif.ImageIFD.Orientation] = 1
+                        exif_bytes = piexif.dump(exif_dict)
+                        img.save(photo.destfullpath, exif=exif_bytes, quality=99)
+            except:
+                pass
             img.close()
 
     def reorderalbumids(self, albums):
